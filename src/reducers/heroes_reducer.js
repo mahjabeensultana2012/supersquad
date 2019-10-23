cfb126f3c7dff6ba20ba1046d9bd3b08fb2f6779
Mahjabeen Sultana <mahjabeen.sultana2012@gmail.com>
2019-10-23 09:52:15 -0500

    combine reducers

---
+++ src/reducers/heroes_reducer.js
@@ -0,0 +1,13 @@
+import { ADD_CHARACTER } from '../actions';
+
+function heroes(state = [], action) {
+  switch (action.type) {
+    case ADD_CHARACTER:
+      let heroes = [...state, createCharacter(action.id)];
+      return heroes;
+    default:
+      return state;
+  }
+}
+
+export default heroes;
