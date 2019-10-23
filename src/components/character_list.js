828646beef99266efaa552e55ad8ff213e180bcd
Mahjabeen Sultana <mahjabeen.sultana2012@gmail.com>
2019-10-23 10:07:33 -0500

    create characterList component

---
+++ src/components/character_list.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class CharacterList extends Component {
+  render() {
+    return (
+      <div>
+        <h4>Characters</h4>
+      </div>
+    );
+  }
+}
+
+export default CharacterList;
