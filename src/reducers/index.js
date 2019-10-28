2857b7854fbbcb1f035237bf7d3132a4be14f2d2
Mahjabeen Sultana <mahjabeen.sultana2012@gmail.com>
2019-10-27 19:17:01 -0500

    connect characterList component

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,4 @@
 import { combineReducers } from 'redux';
-import characters_json from '../data/characters.json';
 
 import characters from './characters_reducer';
 import heroes from './heroes_reducer';
