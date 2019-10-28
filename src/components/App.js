bf9afc244fc295a332a6d031dbf0e6a0c7441a2b
Mahjabeen Sultana <mahjabeen.sultana2012@gmail.com>
2019-10-27 19:27:29 -0500

    mapping state to props

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react';
+import CharacterList from './CharacterList';
 
 class App extends Component {
   render() {
-    return <div>SuperSquad</div>;
+    return (
+      <div>
+        <h2>SuperSquad</h2>
+        <CharacterList />
+      </div>
+    );
   }
 }
 
