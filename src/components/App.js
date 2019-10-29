646eb4ecfa992f51544176d8954aea4b64fee2e2
Mahjabeen Sultana <mahjabeen.sultana2012@gmail.com>
2019-10-29 12:47:11 -0500

    HeroList is working

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import CharacterList from './CharacterList';
+import HeroList from './HeroList';
 import '../styles/index.css';
 
 class App extends Component {
@@ -8,6 +9,7 @@ class App extends Component {
       <div className="App">
         <h2>SuperSquad</h2>
         <CharacterList />
+        <HeroList />
       </div>
     );
   }
