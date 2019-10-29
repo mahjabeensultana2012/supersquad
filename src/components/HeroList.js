2f2fcad26a7608f5678a783fff64130a84e2d188
Mahjabeen Sultana <mahjabeen.sultana2012@gmail.com>
2019-10-29 12:40:41 -0500

    create heroes list

---
+++ src/components/HeroList.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class HeroList extends Component {
+  render() {
+    return (
+      <div>
+        <h4>Your Hero Squad</h4>
+        <ul>
+          {this.props.heroes.map(hero => {
+            return (
+              <li key={hero.id} className="list-group-item">
+                <div className="list-item">{hero.name}</div>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    );
+  }
+}
