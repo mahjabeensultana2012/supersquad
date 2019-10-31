5fb606c438f97937bf2961b9e08434268f956cee
Mahjabeen Sultana <mahjabeen.sultana2012@gmail.com>
2019-10-30 21:29:20 -0500

    add squadStats

---
+++ src/components/SquadStats.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+import {connect} from 'react-redux'
+
+class SquadStats extends Component{
+  render(){
+    return(
+      <div>
+        <h4>Squad/h4></div>
+    )
+  }
+}
