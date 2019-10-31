9b8224978a36f0e57fddac1e28dc311419e76c96
Mahjabeen Sultana <mahjabeen.sultana2012@gmail.com>
2019-10-30 21:31:03 -0500

    add squadStats

--- src/components/SquadStats.js
+++ src/components/SquadStats.js
@@ -1,11 +1,23 @@
-import React, {Component} from 'react'
-import {connect} from 'react-redux'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-class SquadStats extends Component{
-  render(){
-    return(
+class SquadStats extends Component {
+  render() {
+    return (
       <div>
-        <h4>Squad/h4></div>
-    )
+        <h4>Squad</h4>
+      </div>
+    );
   }
 }
+
+function mapStateToProps(state) {
+  return {
+    heroes: state.heroes,
+  };
+}
+
+export default connect(
+  mapStateToProps,
+  null
+)(SquadStats);
